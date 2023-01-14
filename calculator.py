ef918b1839940a488085c7357d188504500ddc03
Ирина <dinimirina@yandex.ru>
2023-01-14 23:27:01 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class :
+    def multiply(self, x, y):
+        return x * y
+
+    def (self, x, y):
+        return x / y
+
+    def (self, x, y):
+        return x - y
+
+    def adding(self, x, y):
+        return x + y
